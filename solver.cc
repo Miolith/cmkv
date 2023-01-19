1f7c1c781a14b09a013f16c5b62f1f2064b23d6f
Nelson VICEL--FARAH <nelson.vicel-farah@epita.fr>
2023-01-19 13:01:18 +0100

    clean code

--- solver.cc
+++ solver.cc
@@ -15,20 +15,20 @@ double get_distance(Board& b, int x, int y)
     {
         int top = tile.top;
         int bot = b.getTile(x, y - 1).bottom;
-        U += bot == top ? 0 : 1;
+        U += bot != top;
     }
     
     if (x > 0)
     {
         int left = tile.left;
         int right = b.getTile(x - 1, y).right;
-        U += left == right ? 0 : 1;
+        U += left != right;
     }
 
     return U;
 }
 
-double get_probability(Board& b)
+double get_global_dist(Board& b)
 {
     double U = 0.0f;
 
@@ -56,36 +56,35 @@ void solver(Board& board)
 {
     int max_iter = 100000000;
     
-    double T_min = pow(0.1, board.dim);
+    double T_min = pow(0.1f, board.dim);
     double T_max = pow(10.0f, board.dim);
-    double T_step = 1.0f - (board.dim + 3)/100.0f;
+    double T_step = (97 - board.dim)/100.0f;
     
     std::random_device rd;  
-	auto g = std::mt19937(rd()); 
-	auto distrib = std::uniform_real_distribution<double>(0.0, 1.0);
+    auto g = std::mt19937(rd()); 
+    auto distrib = std::uniform_real_distribution<double>(0.0, 1.0);
 
     double T = T_max;
-    double U = get_probability(board);
+    double U = get_global_dist(board);
     double U_new = 0.0f;
 
     int reheat = 0;
+    int tile1 = 0;
+    int tile2 = 0;
 
     while(U != 0.0f)
     {
-        int tile1 = 0;
-        int tile2 = 0;
-
-        while (tile1 == tile2 || board[tile2].fixed || board[tile1].fixed)
+        do
         {
             tile1 = rand() % board.size;
             tile2 = rand() % board.size;
-        }
+        } while (board[tile2].fixed || board[tile1].fixed);
 
         std::swap(board[tile1], board[tile2]);
 
-        U_new = get_probability(board);
+        U_new = get_global_dist(board);
 
-        reheat += U_new >= U ? 1 : 0;
+        reheat += U_new >= U;
 
         if (reheat > 100)
         {
